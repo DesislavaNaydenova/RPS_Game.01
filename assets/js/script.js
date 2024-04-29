22f29fbc28336e865b7c07aec7ed3af5d197e3db
Desislava Naydenova <deskata.01@gmail.com>
2024-04-29 10:15:47 +0000

    Add some code to make the game responsive for different screen sizes

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+
+
+// Function to get user input
+function getUserInput() {
+    let userChoice = ;
+    return user_choice;
+  }
+
+
+// Function to get computer move
+function getComputerMove() {
+    computer_choice = "paper";  // todo: randomize computer move
+    return computer_choice;
+  }
+
+
+// Function to compare
+function compareMoves(user_choice, computer_choice) {    
+    // if human = rock and comp = paper
+    //    winner = human
+    winner = "human"
+    return winner;
+  }
+
+function incrementScore() {}
+  
+function checkGameEnd() {}
+  
