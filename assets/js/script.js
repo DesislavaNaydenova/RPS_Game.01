f805d0a06c209ff287b814bbf98b390c9f9c4195
Desislava Naydenova <deskata.01@gmail.com>
2024-04-29 12:03:12 +0000

    Add the functionality for incrementing the score.

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,23 +34,49 @@ function compareMoves(userInput, computerMove) {
 
     let capUserInput = userInput.charAt(0).toUpperCase() + userInput.slice(1);
     let capComputerMove = computerMove.charAt(0).toUpperCase() + computerMove.slice(1);
+    
 
     if(userInput === computerMove){
         action.textContent = `${capUserInput} is the same as ${capComputerMove}`;
-        winner.textContent = "It's a TIE !"
+        winner.textContent = "It's a TIE !";
     } else if (userInput === "rock" && computerMove === "paper"){
         action.textContent = `${capComputerMove} covers ${capUserInput}`;
-        winner.textContent = "Computer wins!"
-
+        winner.textContent = "Computer wins!";
+    } else if (userInput === "rock" && computerMove === "scissors"){
+        action.textContent = `${capUserInput} crushes ${capComputerMove}`;
+        winner.textContent = "Player wins!";
+    } else if (userInput === "paper" && computerMove === "scissors"){
+        action.textContent = `${capComputerMove} cuts ${capUserInput}`;
+        winner.textContent = "Computer wins!";
+    } else if (userInput === "paper" && computerMove === "rock"){
+        action.textContent = `${capUserInput} covers ${capComputerMove}`;
+        winner.textContent = "Player wins!";
+    } else if (userInput === "scissors" && computerMove === "rock"){
+        action.textContent = `${capComputerMove} crushes ${capUserInput}`;
+        winner.textContent = "Computer wins!";
+    } else if (userInput === "scissors" && computerMove === "paper"){
+        action.textContent = `${capUserInput} cuts ${capComputerMove}`;
+        winner.textContent = "Player wins!";
+    } else {
+        action.textContent = "Invalid move";
+        winner.textContent = "";
     }
-    return action;
-    return winner;
-    
-  }
+    incrementScore(winner.textContent);
+    return action;    
+  };
 
 
 // Increment scor depending on the winner
-function incrementScore() {}
+function incrementScore(winner) {
+    let pScore = document.getElementById("player-score");
+    let cScore = document.getElementById("computer-score");
+
+    if( winner === "Player wins!"){
+        pScore.textContent = parseInt(pScore.textContent) +1;
+    }else if (winner === "Computer wins!"){
+        cScore.textContent = parseInt(cScore.textContent) +1;
+    };
+};
 
 
 // End the game when one of the players get 20 points  
@@ -58,7 +84,6 @@ function checkGameEnd() {}
 
 getUserInput();
 getComputerMove();
-console.log(computerMove);
-compareMoves(userInput, computerMove)
+compareMoves();
 
   
